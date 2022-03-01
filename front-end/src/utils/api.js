8606b9e7eaef817220c3e275c7cc20740f049d26
Michael Holthausen <mgholthausen@gmail.com>
2022-03-01 11:00:04 -0500

    fixing comment

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-//Load in API base url
+//Load in API Base Url
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
